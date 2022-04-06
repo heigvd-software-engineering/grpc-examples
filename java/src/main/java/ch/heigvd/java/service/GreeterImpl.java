2a6f03030d08c13b633d789df3f3c45b9e3045e9
James Smith <swerky55@gmail.com>
2022-04-06 09:49:48 +0200

    Init java grpc server example

---
+++ java/src/main/java/ch/heigvd/java/service/GreeterImpl.java
@@ -0,0 +1,12 @@
+package ch.heigvd.java.service;
+
+import io.grpc.stub.StreamObserver;
+
+public class GreeterImpl extends ch.heigvd.java.helloworld.GreeterGrpc.GreeterImplBase {
+  @Override
+  public void sayHello(ch.heigvd.java.helloworld.HelloRequest req, StreamObserver<ch.heigvd.java.helloworld.HelloReply> responseObserver) {
+    ch.heigvd.java.helloworld.HelloReply reply = ch.heigvd.java.helloworld.HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+    responseObserver.onNext(reply);
+    responseObserver.onCompleted();
+  }
+}
